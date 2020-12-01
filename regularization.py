97285e2f674845d84db59cc4ba5d086a01461bf4
mmbejani <mbejani@aut.ac.ir>
2020-12-01 18:23:36 +0330

    v1.0.0

--- regularization.py
+++ regularization.py
@@ -294,7 +294,8 @@ class LRFLoss(nn.Module):
     def forward(self, output, target):
         loss = self.loss_function(output, target)
         theta = self.concat_vectors(self.vectorize_parameters(list(self.net.parameters())))
-        reg = torch.norm(theta - self.theta_star)
+        ts = self.concat_vectors(self.vectorize_parameters(self.theta_star))
+        reg = torch.norm(theta - ts)
         return loss + reg
 
     @staticmethod
